174267a944a011d54b0c3756ec92e8d72431b488
jnicolasdz <jndiazs@udistrital.edu.co>
2026-05-30 08:43:56 -0500

    feat(esp32_webServer/): uploading esp32 web server code with basic endpoints

---
+++ esp32_webServer/esp32_webServer.ino
@@ -0,0 +1,72 @@
+#include <WiFi.h>
+#include <WiFiUdp.h>
+#include <WebServer.h>
+#include <Arduino.h>
+
+#include "services.h"
+
+const char* ssid = "Hidden_Network";
+const char* password = "pass12345";
+
+const IPAddress dest = IPAddress(192, 168, 4, 2);
+const int puerto = 8080;
+const byte macAddress[6] = {0x52,0x17,0x8A,0x22,0x81,0x8D};
+
+WebServer server(80);
+
+void setup() {
+
+    // inicialización de comunicación serial
+    Serial.begin(115200);
+
+    // Configuración red 
+    WiFi.mode(WIFI_AP);
+    WiFi.softAP(ssid, password);
+
+    // declaración de endpoints
+    server.on("/mp", getMagicPacket);
+    server.on("/sendMP", sendMagicPacket); 
+    server.on("/printf", printMensaje);  // sample: /printf?msg=HolaMundo
+
+    server.begin();
+    Serial.println("HTTP server started");
+}
+
+void loop() {
+    server.handleClient();
+}
+
+// Routers
+
+void getMagicPacket() {
+
+  byte magicPacket[102];
+  createMagicPacket(magicPacket, macAddress);
+  String out = "";
+  
+  for(int i = 0; i < 102; i++){
+    out+= String(magicPacket[i], HEX);
+  }
+
+  server.send(200, "text/plain", out);
+}
+
+void sendMagicPacket() {
+
+  byte magicPacket[102];
+  createMagicPacket(magicPacket, macAddress);
+  sendUDP(magicPacket, 102, 7, dest);
+  server.send(200, "text/plain", "Magic packet sent");
+}
+
+void printMensaje() {
+
+  if (!server.hasArg("msg")) {
+    server.send(400, "text/plain", "Missing argument : ?msg=<mensaje>");
+    return;
+  }
+  
+  String mensaje = server.arg("msg");
+  sendTCP(mensaje, dest, puerto);
+  server.send(200, "text/plain", "Printing into client : " + mensaje);
+}
---
+++ esp32_webServer/services.h
@@ -0,0 +1,49 @@
+
+#ifndef SERVICES_H
+
+#define SERVICES_H
+
+#include <cstdint>
+#include <WiFi.h>
+#include <WiFiUdp.h>
+#include <WebServer.h>
+
+void createMagicPacket(uint8_t* magicPacket, const byte* macAddress) {
+
+  int index = 0;
+
+  for(int i=0; i < 6; i++){
+    magicPacket[index++] = 0xFF;
+  }
+
+  for(int i=0; i < 16; i++) {
+    for(int j = 0; j < 6; j++) {
+      magicPacket[index++] = macAddress[j];
+    }
+  }
+}
+
+void sendUDP(const void* data, size_t size, uint16_t puerto, IPAddress dest) {
+    WiFiUDP udp;
+    udp.beginPacket(dest, puerto);
+    udp.write((const uint8_t*) data, size);
+    udp.endPacket();
+}
+
+void sendTCP(String mensaje, IPAddress dest, uint16_t puerto) {
+
+  WiFiClient client;
+  if (client.connect(dest, puerto)) {
+    client.println("POST / HTTP/1.1");
+    client.println("Host: " + String(dest));
+    client.println("Content-Length: " + String(mensaje.length()));
+    client.println("Connection: close");
+    client.println();
+    client.print(mensaje);
+    client.stop();
+  } else {
+    Serial.println("Connection failed to " + dest.toString());
+  }
+}
+
+#endif 
